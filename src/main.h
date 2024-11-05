66fe216a55724736801e7e0e4b4c7f10053883a3
Leandro A. Ibarra <leandroibarra.sog@gmail.com>
2024-11-05 17:17:06 -0300

    Removed comment from vstudio

--- src/main.h
+++ src/main.h
@@ -2,8 +2,6 @@
 #define _MAIN_H
 
 #ifdef ALLEGRO_WINDOWS // MSVC, MinGW, Cygwin
-    // substitutes taken from:
-    // https://social.msdn.microsoft.com/Forums/vstudio/en-US/9356f151-51d2-412d-8889-d53230cc6ec9/porting-srand48-and-drand48-of-unix-like-platform-based-source?forum=vclanguage
     #define SRAND48() srand((long)time(NULL))
     #define DRAND48() ((double)rand() / RAND_MAX)
 #else // OSX, Linux
